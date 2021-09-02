bcebbef0c118aec37a428e77bcf170f6ffedbabb
krishna <gopaljirai1999.com>
2021-09-02 10:41:59 +0530

    edited 3

--- 1-Java/01-java-learn/src/MenuProb.java
+++ 1-Java/01-java-learn/src/MenuProb.java
@@ -3,29 +3,30 @@ public class MenuProb {
 
 	public static void main(String[] args) {
 		String[] menu = {"Balance Enquiry", "Recharge", "Change Caller Tune", "Exit"};
+		
 		int x;
 		do {
 			System.out.println("enter number");
 			Scanner in = new Scanner(System.in);
 			x = in.nextInt();
-			
+		
 			if(x == 1) {
-				System.out.println("Balance Enquiry");
+				System.out.println(menu[x-1]);
 			}
 			else if (x == 2) {
-				System.out.println("Recharge");
+				System.out.println(menu[x-2]);
 			}
 			else if (x == 3) {
-				System.out.println("Change Caller Tune");
+				System.out.println(menu[x-3]);
 			}
 			else if (x == 4) {
-				System.out.println("Exit");
+				System.out.println(menu[x-4]);
 			} 
 			else {
 				System.out.println("Wrong Entered");
 			}
 		} while(x != 4 );
-
+		
 	}
 
 }
