113a03dc45ce1bc46204344740fc4b7eaa7c8c4f
krishna <gopaljirai1999.com>
2021-10-04 12:31:50 +0530

    Customer Signup

--- projects/Project1/bank-app/bank-app-ui/scripts/signup.js
+++ projects/Project1/bank-app/bank-app-ui/scripts/signup.js
@@ -3,8 +3,10 @@ let firstName = document.getElementById("firstName");
 let lastName = document.getElementById("lastName");
 let email = document.getElementById("email");
 let password = document.getElementById("password");
-let error = document.getElementById("error");
-error.style.display = "none";
+let firstNameError = document.getElementById("error");
+
+firstNameError.style.display = "none";
+let valid = true;
 signup.addEventListener("click", function () {
     console.log("clicked");
     if (firstName.value == "") {
@@ -12,37 +14,57 @@ signup.addEventListener("click", function () {
         firstNameError.innerText = "First name is required";
         let valid = false;
     }
-    if (valid) {
-        let customer = {
-            firstName: firstName.value,
-            lastName: lastName.value,
-            email : email.value,
-            password: password.value 
-        };
-
-        var options = {
-            method
+    addCustomer();
+});
+    
+        async function addCustomer(){
+            if (valid) {
+            let customer = {
+                firstName: firstName.value,
+                lastName: lastName.value,
+                email : email.value,
+                password: password.value 
+            };
+    
+            var options = {
+                method: 'POST',
+                headers: { "Content-Type": "application/json"},
+                body: JSON.stringify(customer)
+            };
+            try {
+                let response = await fetch("http://localhost:8080/bank-app-rest/customers", options);
+                //let data = await response.json();
+                console.log("after getting data");
+        
+                console.log("after clearing form");
+        
+            } catch (err) {
+            }    
         }
+        
     }
 
-});
+
+
 
 firstName.addEventListener("keyup", function () {
     //console.log(firstName.value);
     if (firstName.value != "" && firstName.value.length > 20) {
         let firstNameError = document.getElementById("firstNameError");
+        firstNameError.style.display = "block";
         firstNameError.innerText = "First Name cannot exceed 20 characters";
         valid = false;
     }
-    if (firstName.value != "" && firstName.value.length <l= 20) {
+    if (firstName.value != "" && firstName.value.length <= 20) {
         
         firstNameError.innerText = "";
-        valid = faslse;
+        valid = true;
 
     }
     if (firstName.value != "") {
-        let pattern = /^[AZa-z]+$/;
+        let pattern = /^[A-Za-z]+$/;
         if(!firstName.value.match(pattern)) {
+            firstNameError.style.display = "block";
             firstNameError.innerText = "Number Not Allowed";
         }
        
